a43053a300c913490b7824d299352f4e37811583
GleitonFranco <gleitonfranco@gmail.com>
2013-05-27 21:36:26 -0300

    Update JogoGleiton.java

--- core/src/main/java/br/com/drestranho/jogogleiton/core/JogoGleiton.java
+++ core/src/main/java/br/com/drestranho/jogogleiton/core/JogoGleiton.java
@@ -2,6 +2,9 @@ package br.com.drestranho.jogogleiton.core;
 
 import static playn.core.PlayN.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import playn.core.Game;
 import playn.core.Image;
 import playn.core.ImageLayer;
@@ -10,6 +13,9 @@ public class JogoGleiton extends Game.Default {
 	static final float GRAVITY = 64;
 
 	ImageLayer layer;
+	Image inimigo2;
+	List<Inimigo> inimigos;
+	List<Canhao> canhoes; 
 
 	float px, py;
 	float x, y;
@@ -23,17 +29,30 @@ public class JogoGleiton extends Game.Default {
 	@Override
 	public void init() {
 		// create and add background image layer
-		Image bgImage = assets().getImage("images/bg.png");
+		Image bgImage = assets().getImage("images/fundo.png");
 		ImageLayer bgLayer = graphics().createImageLayer(bgImage);
 		graphics().rootLayer().add(bgLayer);
 		
 		x = graphics().width() / 2;
 	    y = graphics().height() / 2;
-	    ay = GRAVITY;
+	    ax = GRAVITY;
 	    
-	    Image img = assets().getImage("images/rat-icon.png");
-	    layer = graphics().createImageLayer(img);
-	    graphics().rootLayer().add(layer);
+	    canhoes = new ArrayList<Canhao>();
+	    canhoes.add(new Canhao(Util.CANHAO_2,Util.Y_CANHAO, "images/canhao.png"));
+	    canhoes.get(0).atirar();
+	    
+//	    inimigo2 = assets().getImage("images/inimigo_2.gif");
+//	    layer = graphics().createImageLayer(inimigo2);
+//	    graphics().rootLayer().add(layer);
+	    inimigos = new ArrayList<Inimigo>();
+	    adicionaInimigo();
+	    
+	    
+	    
+	}
+
+	private void adicionaInimigo() {
+		inimigos.add(new Inimigo(20f,20f,"images/inimigo_2.gif"));
 	}
 
 	@Override
@@ -41,28 +60,59 @@ public class JogoGleiton extends Game.Default {
 		// Save previous position for interpolation.
 	    px = x;
 	    py = y;
+	    
+	    for (Canhao c : canhoes) {
+	    	c.mover();
+	    }
+	    
+	    for (Inimigo i : inimigos) {
+	    	i.mover();
+	    }
 
 	    // Update physics.
-	    delta /= 1000;
-	    vx += ax * delta;
-	    vy += ay * delta;
-	    x += vx * delta;
-	    y += 1;//vy * delta;
+//	    delta /= 1000;
+//	    vx += ax * delta;
+//	    vy += ay * delta;
+//	    x += 1;//vx * delta;
+//	    y += vy * delta;
 	}
 
 	@Override
 	public void paint(float alpha) {
 		// the background automatically paints itself, so no need to do anything here!
 		// Interpolate current position.
-	    float x = (this.x * alpha) + (px * (1f - alpha));
-	    float y = (this.y * alpha) + (py * (1f - alpha));
+//	    float x = (this.x * alpha) + (px * (1f - alpha));
+//	    float y = (this.y * alpha) + (py * (1f - alpha));
 
 	    // Update the layer.
-	    layer.transform();
-	    layer.setTranslation(
-	      x - layer.image().width() / 2,
-	      y - layer.image().height() / 2
-	    );
+		
+		for (Canhao c : canhoes) {
+			c.getLayer().transform();
+	    	c.getLayer().setTranslation(
+	    		      c.getX(),
+	    		      c.getY()
+	    			);
+	    	for (Missil m : c.misseis) {
+	    		m.getLayer().transform();
+		    	m.getLayer().setTranslation(
+		    		      m.getX(),
+		    		      m.getY()
+		    			);
+	    	}
+	    }
+		
+	    for (Inimigo i : inimigos) {
+	    	i.getLayer().transform();
+	    	i.getLayer().setTranslation(
+	    		      i.getX(),
+	    		      i.getY()
+	    			);
+	    }
+//	    layer.transform();	    
+//	    layer.setTranslation(
+//	      x - layer.image().width() / 2,
+//	      y - layer.image().height() / 2
+//	    );
 	}
 	
 	public int updateRate() {
